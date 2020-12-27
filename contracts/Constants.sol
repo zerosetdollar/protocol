1a680fc5f7c847d58c981f96881ad6eda5915e4f
ZeroDev <zerosetfinace@protonmail.com>
2020-12-27 12:52:06 +0700

    Init Commit

---
+++ contracts/Constants.sol
@@ -0,0 +1,162 @@
+/*
+    Copyright 2020 Zero Set Dollar Devs
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "./external/Decimal.sol";
+
+library Constants {
+   /* Chain */
+    uint256 private constant CHAIN_ID = 1; // Mainnet
+
+    /* Bootstrapping */
+    uint256 private constant BOOTSTRAPPING_PERIOD = 126; // 126 epochs
+    uint256 private constant BOOTSTRAPPING_PRICE = 132e16; // 1.32 USDC
+
+    /* Oracle */
+    address private constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    uint256 private constant ORACLE_RESERVE_MINIMUM = 1e10; // 10,000 USDC
+
+    /* Bonding */
+    uint256 private constant INITIAL_STAKE_MULTIPLE = 1e6; // 100 ZSD -> 100M ZSDS
+
+    /* Epoch */
+    struct EpochStrategy {
+        uint256 offset;
+        uint256 start;
+        uint256 period;
+    }
+
+    uint256 private constant EPOCH_OFFSET = 0;
+    uint256 private constant EPOCH_START = 1609005600;
+    uint256 private constant EPOCH_PERIOD = 14400;
+
+    /* Governance */
+    uint256 private constant GOVERNANCE_PERIOD = 18;
+    uint256 private constant GOVERNANCE_QUORUM = 33e16; // 33%
+    uint256 private constant GOVERNANCE_SUPER_MAJORITY = 66e16; // 66%
+    uint256 private constant GOVERNANCE_EMERGENCY_DELAY = 6; // 6 epochs
+
+    /* DAO */
+    uint256 private constant ADVANCE_INCENTIVE = 75e18; // 75 ZSD
+    uint256 private constant DAO_EXIT_LOCKUP_EPOCHS = 24; // 24 epochs fluid
+
+    /* Pool */
+    uint256 private constant POOL_EXIT_LOCKUP_EPOCHS = 9; // 9 epochs fluid
+
+    /* Market */
+    uint256 private constant COUPON_EXPIRATION = 180;
+    uint256 private constant DEBT_RATIO_CAP = 35e16; // 35%
+    uint256 private constant INITIAL_COUPON_REDEMPTION_PENALTY = 50e16; // 50%
+    uint256 private constant COUPON_REDEMPTION_PENALTY_DECAY = 3600; // 1 hour
+
+    /* Regulator */
+    uint256 private constant SUPPLY_CHANGE_DIVISOR = 9e18; // 9%
+    uint256 private constant SUPPLY_CHANGE_LIMIT = 6e16; // 6%
+    uint256 private constant ORACLE_POOL_RATIO = 30; // 30%
+
+    /**
+     * Getters
+     */
+    function getUsdcAddress() internal pure returns (address) {
+        return USDC;
+    }
+
+    function getOracleReserveMinimum() internal pure returns (uint256) {
+        return ORACLE_RESERVE_MINIMUM;
+    }
+
+    function getEpochStrategy() internal pure returns (EpochStrategy memory) {
+        return EpochStrategy({
+            offset: EPOCH_OFFSET,
+            start: EPOCH_START,
+            period: EPOCH_PERIOD
+        });
+    }
+
+    function getInitialStakeMultiple() internal pure returns (uint256) {
+        return INITIAL_STAKE_MULTIPLE;
+    }
+
+    function getBootstrappingPeriod() internal pure returns (uint256) {
+        return BOOTSTRAPPING_PERIOD;
+    }
+
+    function getBootstrappingPrice() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: BOOTSTRAPPING_PRICE});
+    }
+
+    function getGovernancePeriod() internal pure returns (uint256) {
+        return GOVERNANCE_PERIOD;
+    }
+
+    function getGovernanceQuorum() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_QUORUM});
+    }
+
+    function getGovernanceSuperMajority() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_SUPER_MAJORITY});
+    }
+
+    function getGovernanceEmergencyDelay() internal pure returns (uint256) {
+        return GOVERNANCE_EMERGENCY_DELAY;
+    }
+
+    function getAdvanceIncentive() internal pure returns (uint256) {
+        return ADVANCE_INCENTIVE;
+    }
+
+    function getDAOExitLockupEpochs() internal pure returns (uint256) {
+        return DAO_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getPoolExitLockupEpochs() internal pure returns (uint256) {
+        return POOL_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getCouponExpiration() internal pure returns (uint256) {
+        return COUPON_EXPIRATION;
+    }
+
+    function getDebtRatioCap() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: DEBT_RATIO_CAP});
+    }
+    
+    function getInitialCouponRedemptionPenalty() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: INITIAL_COUPON_REDEMPTION_PENALTY});
+    }
+
+    function getCouponRedemptionPenaltyDecay() internal pure returns (uint256) {
+        return COUPON_REDEMPTION_PENALTY_DECAY;
+    }
+
+    function getSupplyChangeLimit() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_LIMIT});
+    }
+
+    function getSupplyChangeDivisor() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_DIVISOR});
+    }
+
+    function getOraclePoolRatio() internal pure returns (uint256) {
+        return ORACLE_POOL_RATIO;
+    }
+
+    function getChainId() internal pure returns (uint256) {
+        return CHAIN_ID;
+    }
+}
---
+++ contracts/dao/Implementation.sol
@@ -0,0 +1,55 @@
+/*
+    Copyright 2020 Zero Set Dollar Devs
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./Market.sol";
+import "./Regulator.sol";
+import "./Bonding.sol";
+import "./Govern.sol";
+import "../Constants.sol";
+
+contract Implementation is State, Bonding, Market, Regulator, Govern {
+    using SafeMath for uint256;
+
+    event Advance(uint256 indexed epoch, uint256 block, uint256 timestamp);
+    event Incentivization(address indexed account, uint256 amount);
+
+    function initialize() initializer public {
+        //250 Zero Dao
+        mintToAccount(0xa6c5c85e1194C0Da3552c6fd8AefA4294209A280 , 250e18);
+        //500 Zero Liquidity
+        mintToAccount(0xeeb8A28123790444588F21958822A7286a28a5F3, 500e18);
+    }
+
+    function advance() external incentivized {
+        Bonding.step();
+        Regulator.step();
+        Market.step();
+
+        emit Advance(epoch(), block.number, block.timestamp);
+    }
+
+    modifier incentivized {
+        // Mint advance reward to sender
+        uint256 incentive = Constants.getAdvanceIncentive();
+        mintToAccount(msg.sender, incentive);
+        emit Incentivization(msg.sender, incentive);
+        _;
+    }
+}
